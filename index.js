5b39cd4bc63203588c6746c5e1928fa6dc7a16ae
Ubuntu <azureuser@vm-demo.irn0kr4uelweromvge1s4ykihc.hydx.internal.cloudapp.net>
2026-07-20 10:17:35 +0000

    separate build and test phase

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const path = require('path');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+const app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Server running on port ${app.get('port')}`);
+});
